bfdf9ede4144abdc7b973a631084325714731996
Ahmed Mady <96249012+ahmed-eid-faried@users.noreply.github.com>
2024-08-10 12:34:15 +0300

    CheckLeapYear2

---
+++ algorithms_problem_solving_level_4/3.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+using namespace std;
+namespace soln3 {
+
+	int ReadNumber(string Message)
+	{
+		int Number;
+		bool IsFinsh = false;
+		while (!IsFinsh)
+		{
+			cout << Message;
+			cin >> Number;
+			if (Number > 0)IsFinsh = true;
+		}
+		return Number;
+
+
+	}
+	bool IsleapYear(short year) {
+ 		return (year % 400 == 0) ? true : (year % 4 == 0 && year % 100 != 0);
+	}
+}
+
+
+void CheckLeapYear2() {
+	short year = soln3::ReadNumber("nEnter a Year ? ");
+	cout << "this year is : ";
+	soln3::IsleapYear(year) ?
+		cout << "Leap Year" << endl :
+		cout << "Common Year" << endl;
+
+}
--- algorithms_problem_solving_level_4/algorithms_problem_solving_level_4.cpp
+++ algorithms_problem_solving_level_4/algorithms_problem_solving_level_4.cpp
@@ -2,12 +2,14 @@
 #include "1_text_of_numbers.h"
 #include "1_soln1_2.h"
 #include "2.h"
+#include "3.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
     //ConvertNumbersToStringEx();
     //ConvertNumbersToStringEx2();
-    CheckLeapYear();
+    //CheckLeapYear();
+    CheckLeapYear2();
 }
  
